ad9dfe7197633d5485de71a1e21d3606f229c48f
Vandon13 <vandon0013@gmail.com>
2020-09-09 20:15:41 +0600

    The road is made, and reset file was added

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ const score = document.querySelector('.score'),
 
 car.classList.add('car');
 
-start.addEventListener('click', startGame);
-document.addEventListener('keydown', startMove);
-document.addEventListener('keyup', stopMove);
-
 const keys = {
 	ArrowUp: false,
 	ArrowDown: false,
@@ -21,29 +17,83 @@ const keys = {
 const settings = {
 	start: false,
 	score: 0,
-	speed: 3
+	speed: 3,
+	traffic: 3
 };
+
+function getAmountElements(heightElement) {
+	return document.documentElement.clientHeight / heightElement + 1;
+}
 
 function startGame() {
 	start.classList.add('hide');
+
+	for (let i = 0; i < getAmountElements(100); i++) {
+		const line = document.createElement('div');
+		line.classList.add('line');
+		line.style.top = (i * 100) + 'px';
+		line.y = i * 100;
+		gameArea.append(line);
+	}
+
 	settings.start = true;
 	gameArea.append(car);
+	settings.x = car.offsetLeft;
+	settings.y = car.offsetTop;
 	requestAnimationFrame(playGame);
 }
 
+function startMove(event) {
+	if (event.key !== 'F5' && event.key !== 'F12') {
+		event.preventDefault();
+		keys[event.key] = true;
+	}
+}
+
+function stopMove(event) {
+	if (event.key !== 'F5' && event.key !== 'F12') {
+		event.preventDefault();
+		keys[event.key] = false;
+	}
+}
+
+function moveRoad() {
+	let lines = document.querySelectorAll('.line');
+	lines.forEach((line) => {
+		line.y += settings.speed;
+		line.style.top = line.y + 'px';
+
+		if (line.y > document.documentElement.clientHeight) {
+			line.y = -100;
+		}
+	});
+}
+
 function playGame() {
-	console.log('Play Game');
 	if (settings.start === true) {
+		moveRoad();
+		if (keys.ArrowLeft === true && settings.x > 0) {
+			settings.x -= settings.speed;
+		}
+
+		if (keys.ArrowRight === true && settings.x < (gameArea.offsetWidth - car.offsetWidth)) {
+			settings.x += settings.speed;
+		}
+
+		if (keys.ArrowUp === true && settings.y > 0) {
+			settings.y -= settings.speed;
+		}
+
+		if (keys.ArrowDown === true && settings.y < (gameArea.offsetHeight - car.offsetHeight - 1)) {
+			settings.y += settings.speed;
+		}
+
+		car.style.left = settings.x + 'px';
+		car.style.top = settings.y + 'px';
 		requestAnimationFrame(playGame);
 	}
 }
 
-function startMove(event) {
-	event.preventDefault();
-	keys[event.key] = true;
-}
-
-function stopMove(event) {
-	event.preventDefault();
-	keys[event.key] = false;
-}
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startMove);
+document.addEventListener('keyup', stopMove);
