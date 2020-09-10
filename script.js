51729eb020bf205dc22077ddc1328a70aca66013
Vandon13 <vandon0013@gmail.com>
2020-09-10 18:26:20 +0600

    The game is done

--- script.js
+++ script.js
@@ -27,7 +27,10 @@ function getAmountElements(heightElement) {
 
 function startGame() {
 	start.classList.add('hide');
-
+	gameArea.innerHTML = '';
+	car.style.left = 125;
+	car.style.top = 'auto';
+	car.style.bottom = '10px';
 	for (let i = 0; i < getAmountElements(100); i++) {
 		const line = document.createElement('div');
 		line.classList.add('line');
@@ -45,7 +48,7 @@ function startGame() {
 		enemy.style.background = 'transparent url(./image/enemy2.png) center / cover no-repeat';
 		gameArea.appendChild(enemy);
 	}
-
+	settings.score = 0;
 	settings.start = true;
 	gameArea.append(car);
 	settings.x = car.offsetLeft;
@@ -82,6 +85,19 @@ function moveRoad() {
 function moveEnemy() {
 	let enemy = document.querySelectorAll('.enemy');
 	enemy.forEach((item) => {
+		let carRect = car.getBoundingClientRect();
+		let enemyRect = item.getBoundingClientRect();
+
+		if (carRect.top <= enemyRect.bottom && 
+			carRect.right >= enemyRect.left && 
+			carRect.left <= enemyRect.right && 
+			carRect.bottom >= enemyRect.top) {
+			settings.start = false;
+			console.warn('ДТП');
+			start.classList.remove('hide');
+			start.style.top = score.offsetHeight;
+		}
+
 		item.y += settings.speed / 2;
 		item.style.top = item.y + 'px';
 	if (item.y >= document.documentElement.clientHeight) {
@@ -93,6 +109,8 @@ function moveEnemy() {
 
 function playGame() {
 	if (settings.start === true) {
+		settings.score += settings.speed;
+		score.innerHTML = 'SCORE: <br>' + settings.score;
 		moveRoad();
 		moveEnemy();
 		if (keys.ArrowLeft === true && settings.x > 0) {
